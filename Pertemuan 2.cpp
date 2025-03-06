7f3d6ec357f375819148bc9a1a2d3321591f2102
GibranSaputra11 <gibranbantul@gmail.com>
2025-03-06 11:14:48 +0700

    Penambahan file pertemuan 2 atau code yang basic

---
+++ Pertemuan 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+    //numeric panjang, lebar, luas
+    double panjang;
+    double lebar;
+    double luas;
+    cout << "Masukan panjangnya = ";
+    //accpet panjang
+    cin >> panjang;
+    cout << "Masukan lebar = ";
+    //accpet panjang
+    cin >> lebar;
+
+    luas = panjang * lebar;
+
+    cout << "Luasnya = " << luas;
+ 
+}
